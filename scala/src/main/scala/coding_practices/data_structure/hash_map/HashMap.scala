60d1e30c39b02699f493396d73771baadb3a831f
papan <papillonbee@gmail.com>
2021-04-08 02:46:47 +0700

    add remove method to hash map

--- scala/src/main/scala/coding_practices/data_structure/hash_map/HashMap.scala
+++ scala/src/main/scala/coding_practices/data_structure/hash_map/HashMap.scala
@@ -11,12 +11,13 @@ case class Key[K](key: K) {
 trait HashMap[K, V] {
   def put(key: K, value: V): HashMap[K, V]
   def get(key: K): Option[V]
+  def remove(key: K): HashMap[K, V]
 }
 
 class HashMapImpl[K, V]() extends HashMap[K, V] {
 
   val loadFactor: Double = 0.7
-  var occupiedIndices: LinkedList[Int] = new LinkedListImpl[Int]()
+  var occupiedAtLeastOnceIndices: LinkedList[Int] = new LinkedListImpl[Int]()
   var lookupTable: Array[LinkedList[(K, V)]] = Array.fill[LinkedList[(K, V)]](1) {
     new LinkedListImpl[(K, V)]()
   }
@@ -28,14 +29,14 @@ class HashMapImpl[K, V]() extends HashMap[K, V] {
     val linkedList: LinkedList[(K, V)] = lookupTable(hashedKey)
 
     if (linkedList.isEmpty) {
-      occupiedIndices.add(hashedKey)
+      occupiedAtLeastOnceIndices.add(hashedKey)
     }
     get(key).map { existingValue: V =>
       linkedList.remove((key, existingValue))
     }
     linkedList.add((key, value))
 
-    if (occupiedIndices.size.toDouble / lookupTable.length > loadFactor) resize()
+    if (occupiedAtLeastOnceIndices.size.toDouble / lookupTable.length > loadFactor) resize()
 
     this
   }
@@ -49,9 +50,20 @@ class HashMapImpl[K, V]() extends HashMap[K, V] {
     valueOpt
   }
 
+  override def remove(key: K): HashMap[K, V] = {
+    get(key).map { value: V =>
+      val hashedKey: Int = key.getHashValue(lookupTable.length)
+      val linkedList: LinkedList[(K, V)] = lookupTable(hashedKey)
+
+      linkedList.remove((key, value))
+    }
+
+    this
+  }
+
   private def resize(): Unit = {
-    val currentOccupiedIndices: LinkedList[Int] = occupiedIndices
-    val newOccupiedIndices: LinkedList[Int] = new LinkedListImpl[Int]()
+    val currentOccupiedAtLeastOnceIndices: LinkedList[Int] = occupiedAtLeastOnceIndices
+    val newOccupiedAtLeastOnceIndices: LinkedList[Int] = new LinkedListImpl[Int]()
 
     val currentLookupTable: Array[LinkedList[(K, V)]] = lookupTable
     val newLookupTable: Array[LinkedList[(K, V)]] = Array.fill[LinkedList[(K, V)]](lookupTable.length * 2) {
@@ -59,19 +71,19 @@ class HashMapImpl[K, V]() extends HashMap[K, V] {
     }
     lookupTable = newLookupTable
 
-    currentOccupiedIndices.map { i: Int =>
+    currentOccupiedAtLeastOnceIndices.map { i: Int =>
       val linkedList: LinkedList[(K, V)] = currentLookupTable(i)
       linkedList.traverse { keyValuePair: (K, V) =>
         val newHashedKey: Int = keyValuePair._1.getHashValue(lookupTable.length)
         val newLinkedList: LinkedList[(K, V)] = newLookupTable(newHashedKey)
 
         if (newLinkedList.isEmpty) {
-          newOccupiedIndices.add(newHashedKey)
+          newOccupiedAtLeastOnceIndices.add(newHashedKey)
         }
         newLinkedList.add(keyValuePair)
       }
     }
 
-    occupiedIndices = newOccupiedIndices
+    occupiedAtLeastOnceIndices = newOccupiedAtLeastOnceIndices
   }
 }
--- scala/src/test/scala/coding_practices/data_structure/hash_map/HashMapTest.scala
+++ scala/src/test/scala/coding_practices/data_structure/hash_map/HashMapTest.scala
@@ -23,7 +23,7 @@ class HashMapTest extends AnyFunSpec
       valueOpt.value shouldEqual value
     }
 
-    it("should put existing key and overwrite existing value with new value correctly") {
+    it("should put existing key and overwrite existing value with new value") {
       val key: String = "myFirstKey"
       val value1: Dinosaur = Dinosaur("John", 22)
       val value2: Dinosaur = Dinosaur("Paul", 5)
@@ -39,6 +39,20 @@ class HashMapTest extends AnyFunSpec
       val key: String = "myFirstKey"
 
       val valueOpt: Option[Dinosaur] = hashMap.get(key)
+      valueOpt shouldBe empty
+    }
+
+    it("should remove key and value correctly") {
+      val key1: String = "myFirstKey"
+      val value1: Dinosaur = Dinosaur("John", 22)
+      val key2: String = "mySecondKey"
+      val value2: Dinosaur = Dinosaur("Paul", 5)
+
+      hashMap.put(key1, value1)
+      hashMap.put(key2, value2)
+      hashMap.remove(key1)
+
+      val valueOpt: Option[Dinosaur] = hashMap.get(key1)
       valueOpt shouldBe empty
     }
   }
