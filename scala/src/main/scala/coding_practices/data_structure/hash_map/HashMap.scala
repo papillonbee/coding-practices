4c8ab26edaac5b6e7e44148ebbdb95875fe6ee4e
papan <papillonbee@gmail.com>
2021-04-08 00:39:10 +0700

    add resize implementation to hash map

--- scala/src/main/scala/coding_practices/data_structure/hash_map/HashMap.scala
+++ scala/src/main/scala/coding_practices/data_structure/hash_map/HashMap.scala
@@ -15,7 +15,9 @@ trait HashMap[K, V] {
 
 class HashMapImpl[K, V]() extends HashMap[K, V] {
 
-  var lookupTable: Array[LinkedList[(K, V)]] = Array.fill[LinkedList[(K, V)]](100) {
+  val loadFactor: Double = 0.7
+  var occupiedIndices: LinkedList[Int] = new LinkedListImpl[Int]()
+  var lookupTable: Array[LinkedList[(K, V)]] = Array.fill[LinkedList[(K, V)]](1) {
     new LinkedListImpl[(K, V)]()
   }
 
@@ -24,17 +26,49 @@ class HashMapImpl[K, V]() extends HashMap[K, V] {
   override def put(key: K, value: V): HashMap[K, V] = {
     val hashedKey: Int = key.getHashValue(lookupTable.length)
     val linkedList: LinkedList[(K, V)] = lookupTable(hashedKey)
+
+    if (linkedList.isEmpty) {
+      occupiedIndices.add(hashedKey)
+    }
     linkedList.add((key, value))
+
+    if (occupiedIndices.size.toDouble / lookupTable.length > loadFactor) resize()
 
     this
   }
 
   override def get(key: K): Option[V] = {
-    val hashValue: Int = key.getHashValue(lookupTable.length)
-    val linkedList: LinkedList[(K, V)] = lookupTable(hashValue)
+    val hashedKey: Int = key.getHashValue(lookupTable.length)
+    val linkedList: LinkedList[(K, V)] = lookupTable(hashedKey)
     val keyValuePairOpt: Option[(K, V)] = linkedList.findBy(p => p._1 == key)
     val valueOpt: Option[V] = keyValuePairOpt.map(_._2)
 
     valueOpt
+  }
+
+  private def resize(): Unit = {
+    val currentOccupiedIndices: LinkedList[Int] = occupiedIndices
+    val newOccupiedIndices: LinkedList[Int] = new LinkedListImpl[Int]()
+
+    val currentLookupTable: Array[LinkedList[(K, V)]] = lookupTable
+    val newLookupTable: Array[LinkedList[(K, V)]] = Array.fill[LinkedList[(K, V)]](lookupTable.length * 2) {
+      new LinkedListImpl[(K, V)]()
+    }
+    lookupTable = newLookupTable
+
+    currentOccupiedIndices.map { i: Int =>
+      val linkedList: LinkedList[(K, V)] = currentLookupTable(i)
+      linkedList.traverse { keyValuePair: (K, V) =>
+        val newHashedKey: Int = keyValuePair._1.getHashValue(lookupTable.length)
+        val newLinkedList: LinkedList[(K, V)] = newLookupTable(newHashedKey)
+
+        if (newLinkedList.isEmpty) {
+          newOccupiedIndices.add(newHashedKey)
+        }
+        newLinkedList.add(keyValuePair)
+      }
+    }
+
+    occupiedIndices = newOccupiedIndices
   }
 }
